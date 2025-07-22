c8a5ee9e1683dd975ad5f1dde65a81dbc72768d1
Niranjan0524 <parthalase05gmali.com@gmail.com>
2025-07-22 09:17:33 +0530

    kruskals algo

---
+++ Graphs/Kruskals.cpp
@@ -0,0 +1,96 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class DisjointSet{
+  //for union find (rank):
+  vector<int> rank;
+  vector<int> parent;
+  //for union find (size);
+  vector<int> size;
+  public:
+
+    DisjointSet(int n){
+        rank.resize(n+1, 0);
+        parent.resize(n+1);
+        size.resize(n+1, 1);
+        for(int i = 0; i <= n; i++){
+            parent[i] = i;
+        }
+    }
+
+    int findParent(int node){
+      if(node==parent[node]) return node;
+
+      return parent[node]=findParent(parent[node]);
+    }
+
+    void unionByRank(int u,int v){
+      int ulP_u=findParent(u);
+      int ulP_v=findParent(v);
+
+      if(ulP_u == ulP_v) return;
+
+      if(rank[ulP_u] < rank[ulP_v]){
+        parent[ulP_u] = ulP_v;
+      }else if(rank[ulP_u] > rank[ulP_v]){
+        parent[ulP_v] = ulP_u;
+      }else{
+        parent[ulP_v] = ulP_u;
+        rank[ulP_u]++;
+      }
+    }
+
+    void unionBySize(int u,int v){
+      int ulP_u=findParent(u);
+      int ulP_v=findParent(v);
+
+      if(ulP_u == ulP_v) return;
+
+      if(size[ulP_u] < size[ulP_v]){
+        parent[ulP_u] = ulP_v;
+        size[ulP_v] += size[ulP_u];
+      }
+      else{
+        parent[ulP_v] = ulP_u;
+        size[ulP_u] += size[ulP_v];
+      }
+    }
+};
+
+class Kruskals{
+ 
+  public:
+
+    int findMST(int v,vector<vector<pair<int,int>>> &adj){
+        vector<pair<int,pair<int,int>>> edges;
+        DisjointSet ds(v);
+
+        for(int i=0;i<v;i++){
+          for(auto it:adj[i]){
+            int node=i;
+            int adjNode=it.first;
+            int weight=it.second;
+            edges.push_back({weight,{node,adjNode}}); 
+          }
+        }
+
+        sort(edges.begin(),edges.end());
+        int totalWeight=0;
+        vector<pair<int,int>> mstEdges;
+
+        for(auto &it:edges){
+          int wt=it.first;
+          int node=it.second.first;
+          int adjNode=it.second.second;
+
+          if(ds.findParent(node) != ds.findParent(adjNode)){
+            mstEdges.push_back({node,adjNode});
+            totalWeight += wt;
+            ds.unionByRank(node,adjNode);
+          }
+        }
+        return totalWeight;
+    } 
+
+}
